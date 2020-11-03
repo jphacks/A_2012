919d63717d771286a106be8c9f61f49148006500
himantyu-yuma <lipton.happy.milktea@gmail.com>
2020-11-03 16:21:05 +0900

    cors対応（多分）

--- kasahara/api_test.py
+++ kasahara/api_test.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def hello():
     name = request.args.get('query', '')
     return name
+
 
 if __name__ == "__main__":
     app.run(debug=True)
