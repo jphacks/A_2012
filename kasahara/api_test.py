8f9d7e3d36f8270ff01768ebaae844fd1afdf92e
himantyu-yuma <lipton.happy.milktea@gmail.com>
2020-11-03 16:24:16 +0900

    対応方法変更

--- kasahara/api_test.py
+++ kasahara/api_test.py
@@ -4,6 +4,16 @@ app = Flask(__name__)
 CORS(app)
 
 
+@api.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
 @app.route('/')
 def hello():
     name = request.args.get('query', '')
