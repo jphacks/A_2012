ac174443254570b1252c6ac635b400a142a889a1
himantyu-yuma <lipton.happy.milktea@gmail.com>
2020-11-03 16:26:31 +0900

    タイポ修正

--- kasahara/api_test.py
+++ kasahara/api_test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-@api.after_request
+@app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers',
