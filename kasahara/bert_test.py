97ca9f48a7fe5023cde11a4d41eaf9671afb2423
himantyu-yuma <lipton.happy.milktea@gmail.com>
2020-11-06 07:29:54 +0900

    sst-2でファインチューニングしたモデルをtensorflow.jsで扱えるように

--- kasahara/bert_test.py
+++ kasahara/bert_test.py
@@ -4,10 +4,11 @@ import tensorflowjs as tfjs
 import numpy as np
 
 tokenizer = AutoTokenizer.from_pretrained("cl-tohoku/bert-base-japanese-whole-word-masking")
-model = AutoModel.from_pretrained("D:/Documents/A_2012/DistilBERT-base-jp")
+# model = AutoModel.from_pretrained("D:/Documents/A_2012/DistilBERT-base-jp")
 # model = TFAutoModel.from_pretrained("D:/Documents/A_2012/DistilBERT-base-jp", from_pt=True)
+model = TFAutoModel.from_pretrained("D:\Documents\A_2012\SentimentAnalysys", from_pt=True)
 
-# model.save_pretrained('test')
+model.save_pretrained('tfjs-test')
 
-# tfjs.converters.save_keras_model(model, 'tfjs')
-print(tokenizer.tokenize('ワイモバとUQ月4000円の新プランで国の要請に対応 アップル歴40年初めてのApple Watch体験 ほか『今週のASCII.jp注目ニュース ベスト5 』'))
+# tfjs.converters.save_keras_model(model, 'tfjs-model')
+# print(tokenizer.tokenize('ワイモバとUQ月4000円の新プランで国の要請に対応 アップル歴40年初めてのApple Watch体験 ほか『今週のASCII.jp注目ニュース ベスト5 』'))
